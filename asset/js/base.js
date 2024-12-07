1530b6baf0f126c88299ef5133eaaf4b7760bebe
olindo55 <julien.martinati@gmail.com>
2024-12-07 12:27:10 +0000

    fix variable flash Alert in base.php

--- asset/js/base.js
+++ asset/js/base.js
@@ -2,7 +2,6 @@ import MyToast from './class/MyToast.js';
 
 // toast flashMessage
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Creating toast');
     if (flashMessage) {
         const toast = new MyToast(flashMessage, flashAlert);
         toast.show();
